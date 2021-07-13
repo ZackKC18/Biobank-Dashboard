dd7d26d41e9ee3688ad2c15142377b25ff4db1d5
TanatchaT <51393042+TanatchaT@users.noreply.github.com>
2021-07-13 15:52:08 +0700

    Add files via upload

---
+++ random2.R
@@ -0,0 +1,35 @@
+library(dplyr)
+library(tidyverse)
+library(tidyr)
+
+#import data
+BS1001 <- read.csv("C:/data/BS1001data.csv")
+BS1101 <- read.csv("C:/data/BS1101data.csv")
+BS1103 <- read.csv("C:/data/BS1103data.csv")
+BS1202 <- read.csv("C:/data/BS1202data.csv")
+BS1203 <- read.csv("C:/data/BS1203data.csv")
+BS1402 <- read.csv("C:/data/BS1402data.csv")
+BS1701 <- read.csv("C:/data/BS1701data.csv")
+BS1904 <- read.csv("C:/data/BS1904data.csv")
+
+#concatenate data
+data <- rbind(BS1001,BS1101,BS1103,BS1202,BS1203,BS1402,BS1701,BS1904)
+#replace blank value with NA value
+data[data==""] <- NA
+colSums(is.na(data))
+
+#Change some column name
+names(data)[14] <- "Project"
+#rename.values(data,'Buffy Coat'='Buffy_Coat') because SQL is sensitive
+data$Specimen_Type <- as.character(data$Specimen_Type)
+data[data == "Buffy Coat"] <- "Buffy_Coat"
+data[data == "Fresh Tissue"] <- "Fresh_Tissue"
+
+#extract year from datetime
+data$Year <- format(as.Date(data$Specimen_Collection.Date,format="%d/%m/%Y"),"%Y" )
+data <- select(data, Participant_PPID,Specimen_Type,Specimen_Pathological.Status, Specimen_Barcode, Specimen_Container.Name, Specimen_Container.Position, Project,Year,Participant_Gender)
+
+set.seed(12345)
+
+randdata <- sample_n(data, 45)
+randdata
