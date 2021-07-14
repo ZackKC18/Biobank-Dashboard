dabaf64ba7414212965b5441f58b8f6ae6faa076
Thanrada Popraros <79976554+ThanradaP@users.noreply.github.com>
2021-07-14 15:16:11 +0700

    Update Percent_Export.R

--- Percent_Export.R
+++ Percent_Export.R
@@ -14,7 +14,6 @@ BS1904 <- read.csv("https://www.dropbox.com/s/t3b4ya6cqo19o95/BS1904data.csv?dl=
 #concatenate raw data
 data <- bind_rows(BS1001,BS1101,BS1103,BS1202,BS1203,BS1402,BS1701,BS1904)
 
-
 #import raw data of transfer dataset
 BS1001_1st_transfer <- read.csv("https://www.dropbox.com/s/39e6df37mc3ymfl/BS1001_1st_transfer_2020.csv?dl=1")
 BS1001_2nd_transfer <- read.csv("https://www.dropbox.com/s/1r8euv513ykkefk/BS1001_2nd_transfer_2020.csv?dl=1")
@@ -25,17 +24,27 @@ BS1204_1st_transfer <- read.csv("https://www.dropbox.com/s/squzceb7eje150c/BS120
 #combine transfer dataset
 data_transfer <- rbind(BS1001_1st_transfer,BS1001_2nd_transfer,BS1103_1st_transfer, BS1203_1st_transfer, BS1204_1st_transfer)
 
-
 #extract year from datetime
 data$Year <- format(as.Date(data$Specimen_Collection.Date,format="%d/%m/%Y"),"%Y" )
 data_transfer$Year <- format(as.Date(data_transfer$Exported.On,format="%m/%d/%Y"),"%Y" )
 
 #summary from raw data
-mytable <- data %>% count(Year) %>% rename(Total_Sample = n) %>% mutate(Cumulative_Sample = cumsum(Total_Sample))
+mytable <- data %>% count(Year) %>%
+  rename(Total_Sample = n)
+
 #summary from transfer data
-trans <- data_transfer %>% count(Year) %>% rename(Total_Transfer = n)
+trans <- data_transfer %>% 
+  count(Year) %>% 
+  rename(Total_Transfer = n)
+
 #combine raw data with transfer data
-merge <- merge(x=mytable,y=trans,by="Year",all.x=TRUE) %>% mutate(Total_Transfer = replace(Total_Transfer, is.na(Total_Transfer),0))
-#calculate percent of total tranfer by year
-final_table <- merge %>% mutate(Total_Transfer = replace(Total_Transfer, is.na(Total_Transfer),0)) %>%  mutate(Percent_Transfer =Total_Transfer/Cumulative_Sample*100 ) 
+merge <- merge(x=mytable,y=trans,by="Year",all.x=TRUE)  %>% 
+  mutate(Total_Transfer=replace_na(Total_Transfer,0)) %>% 
+  mutate(Net_Sample = ifelse(Year == Year[1], 
+                             Total_Sample,Total_Sample-dplyr::lag(Total_Transfer))) %>% 
+  mutate(Cumulative_Sample = cumsum(Net_Sample))
+
+#calculate percent of total tranหfer by year
+final_table <- merge %>%  
+  mutate(Percent_Transfer =Total_Transfer/Cumulative_Sample*100 ) 
 
